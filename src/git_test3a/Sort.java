c4676ddef033776744bee0b6228c586ed687cbc1
nmmn1004 <jjj4120@gmail.com>
2024-04-17 12:52:33 +0900

    quick_sort 구현 완성

--- src/git_test3a/Sort.java
+++ src/git_test3a/Sort.java
@@ -13,7 +13,13 @@ public class Sort {
     }
 
     static int partition(int[] a, int start, int end) {
-        // TODO: partition 알고리즘을 구현해야 함
+        int value = a[end];
+        int i = start - 1;
+        for (int j = start; j <= end - 1; ++j)
+            if (a[j] < value)
+                swap(a, ++i, j);
+        swap(a, i + 1, end);
+        return i + 1;
     }
 
     static void quickSort(int[] a, int start, int end) {
