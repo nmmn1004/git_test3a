f433d1b0814c99dec05213e552268a8ec7a993ff
nmmn1004 <jjj4120@gmail.com>
2024-04-17 12:57:44 +0900

    랜덤 데이터

--- src/git_test3a/Main.java
+++ src/git_test3a/Main.java
@@ -1,9 +1,20 @@
 package git_test3a;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
 
-    public static void main(String[] args) {
-
+    static int[] createArray(int size, int minValue, int maxValue) {
+        Random random = new Random();
+        int[] a = new int[size];
+        for (int i = 0; i < a.length; ++i)
+            a[i] = random.nextInt(maxValue - minValue + 1) + 1;
+        return a;
     }
 
+    public static void main(String[] args) {
+        int[] a = createArray(10, 1, 10);
+        System.out.println(Arrays.toString(a));
+    }
 }
